41bb4242918d33c1a95d1d328cf312e61197e52c
José Luís Gomes <joslus.luis@gmail.com>
2022-07-06 11:48:31 +0100

    Vehicle's repository tests done.

--- src/test/java/com/example/stand/vehicle/VehicleRepositoryTest.java
+++ src/test/java/com/example/stand/vehicle/VehicleRepositoryTest.java
@@ -7,10 +7,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
-import java.util.Optional;
+import java.util.List;
 
 import static com.example.stand.vehicle.engine.Fuel.DIESEL;
-import static com.example.stand.vehicle.engine.Motor.CAR;
+import static com.example.stand.vehicle.engine.Fuel.GASOLINE;
+import static com.example.stand.vehicle.engine.Motor.*;
 
 @DataJpaTest
 class VehicleRepositoryTest {
@@ -37,14 +38,70 @@ class VehicleRepositoryTest {
     }
 
     @Test
-    void getByBrandAndModel() {
+    @DisplayName(value = "Should return a list with the vehicles from the given brand and model")
+    void shouldGetTheVehiclesOfTheBrandAndModel() {
+        // given
+        final var vehicle1 = new Vehicle
+                .Builder(1L, "BMW", "0123456789", 5, JEEP, GASOLINE)
+                .model("X3")
+                .build();
+        final var vehicle2 = new Vehicle
+                .Builder(2L, "BMW", "0125456785", 5, CAR, DIESEL)
+                .model("X3")
+                .build();
+        underTest.saveAll(List.of(vehicle1, vehicle2));
+
+        // when
+        final var vehiclesPretended = underTest
+                .getByBrandAndModel("BMW", "X3")
+                .toArray();
+
+        // then
+        AssertionsForClassTypes.assertThat(vehiclesPretended)
+                .containsAll(List.of(vehicle1, vehicle2));
     }
 
     @Test
-    void getByMotor() {
+    @DisplayName(value = "Should return a list with all the vehicles which contain the given motor")
+    void getVehiclesByMotor() {
+        // given
+        final var vehicle1 = new Vehicle
+                .Builder(1L, "BMW", "0123456789", 5, VAN, GASOLINE)
+                .model("X3")
+                .build();
+        final var vehicle2 = new Vehicle
+                .Builder(2L, "BMW", "0125456785", 5, VAN, DIESEL)
+                .model("X3")
+                .build();
+        underTest.saveAll(List.of(vehicle1, vehicle2));
+
+        // when
+        final var vehiclesPretended = underTest.getByMotor(VAN).toArray();
+
+        // then
+        AssertionsForClassTypes.assertThat(vehiclesPretended)
+                .containsAll(List.of(vehicle1, vehicle2));
     }
 
     @Test
-    void getByFuel() {
+    @DisplayName(value = "Should return a list with all the vehicles which contain the given fuel")
+    void getVehiclesByFuel() {
+        // given
+        final var vehicle1 = new Vehicle
+                .Builder(1L, "BMW", "0123456789", 5, VAN, GASOLINE)
+                .model("X3")
+                .build();
+        final var vehicle2 = new Vehicle
+                .Builder(2L, "BMW", "0125456785", 5, VAN, GASOLINE)
+                .model("X3")
+                .build();
+        underTest.saveAll(List.of(vehicle1, vehicle2));
+
+        // when
+        final var vehiclesPretended = underTest.getByFuel(GASOLINE).toArray();
+
+        // then
+        AssertionsForClassTypes.assertThat(vehiclesPretended)
+                .containsAll(List.of(vehicle1, vehicle2));
     }
 }
